ee6a82dc64dd414b082991855a307ee14a49f5d9
qianxunslimg <2511167079@qq.com>
2022-01-28 16:04:50 +0000

    init

---
+++ 作业情况统计/PIGAI.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "ui_PIGAI.h"
+#include <QtWidgets/QWidget>
+
+class PIGAI : public QWidget {
+  Q_OBJECT
+
+public:
+  PIGAI(QWidget *parent = Q_NULLPTR);
+
+private:
+  Ui::PIGAIClass ui;
+  QVector<QPair<QString, double>> all_name_score;
+  QVector<QPair<QString, double>> blank_name_score;
+  QVector<QPair<QString, double>> choice_name_score;
+  QString filename;
+  QVector<QString> finalNameList;
+  void showName(QVector<QString>);
+
+  void sortByOrderAndSub(int, int);
+public slots:
+  void OnBtOpenClicked();
+  void OnCbOrderChanged(int k);
+  void OnCbSubChanged(int k);
+  void SpinChanged(int k);
+};
